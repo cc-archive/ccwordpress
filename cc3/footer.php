724a7566b5d5ba2d0b6ee83eeefa3e6406c187c1
mlinksva <mlinksva@committer.creativecommons.org>
2007-02-23 01:17:56 +0000

    3.0

--- cc3/footer.php
+++ cc3/footer.php
@@ -27,9 +27,9 @@
         <div class="clear">&nbsp;</div>
       </div>
       <p>
-<a rel="license" href="http://creativecommons.org/licenses/by/2.5/"><img src="http://i.creativecommons.org/l/by/2.5/88x31.png" alt="Creative Commons License" style="border: medium none ;" height="31" width="88"></a><br>
+<a rel="license" href="http://creativecommons.org/licenses/by/3.0/"><img src="http://i.creativecommons.org/l/by/3.0/88x31.png" alt="Creative Commons License" style="border: medium none ;" height="31" width="88"></a><br>
 Except where otherwise <a class="subfoot" href="/policies#license">noted</a>, content on this site is<br/>
-licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/2.5/" class="subfoot">Creative Commons Attribution 2.5 License</a>
+licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/3.0/" class="subfoot">Creative Commons Attribution 3.0 License</a>
       <p>
     </div>
   </div>
