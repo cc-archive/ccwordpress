58c70cbd9c41e68a6e8762711d7658dde1a1ad82
bryce <bryce@committer.creativecommons.org>
2007-03-21 21:48:44 +0000

    Implementing category listing index page

--- cc3/category-21.php
+++ cc3/category-21.php
@@ -1,19 +1,33 @@
-<?php /* Featured Projects category page */ ?>
+<?php /* License jurisdiction category page */ ?>
+
+<?php function get_the_jurisdiction($post_id) {
+    $cats = get_the_category($post_id);
+    $jurisdiction->code = '';
+    $jurisdiction->status = '';
+    foreach ( $cats as $cat ) {
+       if ( $cat->category_parent == 21 ) {
+           $jurisdiction->code = $cat->category_nicename;
+           $jurisdiction->name = $cat->cat_name;
+       }
+       if ( $cat->category_parent == 17 ) {
+           $jurisdiction->status = $cat->category_nicename;
+       }
+       if ( $cat->cat_ID == 18 ) {
+           $jurisdiction->status = 'upcoming';
+       } else if ( $cat->cat_ID == 19 ) {
+           $jurisdiction->status = 'in-progress';
+       } else if ( $cat->cat_ID == 20 ) {
+           $jurisdiction->status = 'completed';
+       }
+    }
+    return $jurisdiction;
+} ?>
 
 <?php get_header(); ?>
 
     <div id="body">
       <div id="splash">
-        <!--img src="images/info.png" align="left"/-->
-        <h2>
-        <? if (is_month() || is_year()) { ?> 
-          <a href="<?php echo get_settings('home') . "/"?>jurisdictions/">
-            Jurisdictions
-          </a>
-        <? } else { ?>
-          Jurisdictions
-        <? } ?>
-        <br/>&nbsp;</h2>
+        <h2>Worldwide<br />&nbsp;</h2>
         <div id="splash-menu">
         </div>
       </div>
@@ -21,28 +35,68 @@
       <div id="content">
         <div id="main-content">
           <div id="blog">
-<?php if (have_posts())  { ?>
-<?php while (have_posts()) { the_post(); ?>
-            <div class="post" id="post-<?php the_ID(); ?>">
-              <h4 class="meta"><?php the_time('F jS, Y')?></h4><h3><a href="<?= get_post_meta ($post->ID, "url", TRUE)?>"><?= $post->post_title ?></a></h3>
-              <div class="clearer"></div>
-              <? if ($attach = cc_get_attachment ($post->ID)) { ?>
-                <a href="<?= get_post_meta ($post->ID, "url", TRUE)?>"><img src="<?= $attach->uri ?>" alt="<?= $post->post_title ?>" title="<?= $post->post_title ?>" border="0" align="left" style="margin-bottom: 30px; margin-right: 10px;" width="160" /></a>
-              <? } ?>
-<div class="excerpt">
-              <?php the_content("Read More..."); ?>
-              <?php edit_post_link('Edit', '', ''); ?>
-</div>
+            <p>We are excited to announce Creative Commons International &mdash; an offshoot of our licensing project dedicated to the drafting and eventual adoption of jurisdiction-specific licenses. Creative Commons International is being lead by <a href="http://creativecommons.org/about/people#65">Catharina Maracke</a> (<a href="mailto:catharina@creativecommons.org">email</a>), with help from member jurisdictions.</p>
+            <h3>Completed Licenses</h3>
+            <p>We have completed the process and developed licenses for the following jurisdictions:</p>
+<?php 
+    query_posts("orderby=title&order=ASC&posts_per_page=-1");
+    if (have_posts())  { ?>
+            <div class="icontainer">
+
+<?php while (have_posts()) { the_post();  
+    $jurisdiction = get_the_jurisdiction($post->ID);
+
+    if ($jurisdiction->code == '' || $jurisdiction->status != 'completed') {
+       continue;
+    }
+    $url = get_post_meta ($post->ID, "url", TRUE);
+    $img = "/images/international/$jurisdiction->code.png";
+?>
+              <div class="ifloat"><a href="<?= $url ?>"><img border="1" src="<?= $img ?>" alt="<?= $jurisdiction->name ?>" style="border:1px solid black'" /></a><br /><p><a href="<?= $url ?>"><?= $jurisdiction->name ?></a></p></div>
+<?php } ?>
             </div>
-<?php } }?>
-            <?php posts_nav_link(' &mdash; ', 'previous page', 'next page'); ?>
+            <br clear="all" />
+
+            <h3>Project Jurisdictions</h3>
+
+            <p>The process of developing licenses and discussing them are still in progress for the following jurisdictions:</p>
+
+            <div class="icontainer">
+<?php rewind_posts(); while (have_posts()) { the_post();
+    $jurisdiction = get_the_jurisdiction($post->ID);
+    if ($jurisdiction->code == '' || $jurisdiction->status != 'in-progress') {
+       continue;
+    }
+    $url = get_post_meta ($post->ID, "url", TRUE);
+    $img = "/images/international/$jurisdiction->code.png";
+?>
+              <div class="ifloat"><a href="<?= $url ?>"><img border="1" src="<?= $img ?>" alt="<?= $jurisdiction->name ?>" style="border:1px solid black'" /></a><br /><p><a href="<?= $url ?>"><?= $jurisdiction->name ?></a></p></div>
+<?php } ?>
+            </div>
+<br clear="all" />
+<a name="more"></a>
+
           </div>
           <div id="features">
-            <h4>Archives</h4>
-            <ul class="archives">
-              <?php cc_get_cat_archives(2, 'monthly', '', 'html', '', '', TRUE); ?>
+            <h4>Upcoming Project Jurisdictions</h4>
+            <ul>
+<?php rewind_posts(); while (have_posts()) { the_post();
+    $jurisdiction = get_the_jurisdiction($post->ID);
+    if ($jurisdiction->code == '' || $jurisdiction->status != 'upcoming') {
+       continue;
+    }
+?>
+              <li><strong><?= $jurisdiction->name ?></strong>: $jurisdiction->excerpt</li>
+<?php } ?>
+            </ul>
+            <br />
+
+            <h4>Upcoming Launch Dates</h4>
+            <ul>
+              <li>Various:  TBD</li>
             </ul>
           </div>
         </div>  
+<?php } ?>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
