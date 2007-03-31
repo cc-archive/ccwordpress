5a48d7a0e0f17374e7d58f983b805a35e41316ff
bryce <bryce@committer.creativecommons.org>
2007-03-31 06:00:09 +0000

    Fixing alt tag on flags

--- cc3/single.php
+++ cc3/single.php
@@ -56,7 +56,7 @@
 	<? if ($is_worldwide) { ?>
         <h2> 
           <img src="/images/international/<?php echo $jurisdiction_code ?>.png" 
-               alt="<?php $jurisdiction_code ?> flag" /><?php 
+               alt="<?php echo $jurisdiction_code ?> flag" style="border:1px solid black'" /><?php 
           the_title(); ?><br/>&nbsp;
         </h2>
 	<? } else { ?>
