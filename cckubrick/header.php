0075973c745354559bc9a0ff6e808aeb67d282f7
bse <bse@committer.creativecommons.org>
2007-04-09 23:32:58 +0000

    Creative Commons restyling.

--- cckubrick/header.php
+++ cckubrick/header.php
@@ -4,26 +4,18 @@
 <head profile="http://gmpg.org/xfn/11">
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 
-<title><?php bloginfo('name'); ?> <?php if ( is_single() ) { ?> &raquo; Blog Archive <?php } ?> <?php wp_title(); ?></title>
+<title>
+	<?php if ( is_single() ) { ?> &raquo; Blog Archive <?php } ?> 
+	<?php wp_title(' '); ?>
+	<?php if(wp_title(' ', false)) { echo ' &mdash; '; } ?> 
+	<?php bloginfo('name'); ?>
+</title>
 
 <meta name="generator" content="WordPress <?php bloginfo('version'); ?>" /> <!-- leave this for stats -->
 
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-
-<style type="text/css" media="screen">
-
-<?php
-// Checks to see whether it needs a sidebar or not
-if ( !$withcomments && !is_single() ) {
-?>
-	#page { background: url("<?php bloginfo('stylesheet_directory'); ?>/images/kubrickbg-<?php bloginfo('text_direction'); ?>.jpg") repeat-y top; border: none; }
-<?php } else { // No sidebar ?>
-	#page { background: url("<?php bloginfo('stylesheet_directory'); ?>/images/kubrickbgwide.jpg") repeat-y top; border: none; }
-<?php } ?>
-
-</style>
 
 <?php wp_head(); ?>
 </head>
