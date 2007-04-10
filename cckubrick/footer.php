bedef3cd4c5c7389cc051fac0733354eec2bccb6
nyergler <nyergler@committer.creativecommons.org>
2007-04-10 20:58:14 +0000

    Removed "new blog" link.

--- cckubrick/footer.php
+++ cckubrick/footer.php
@@ -5,7 +5,7 @@
 <!-- If you'd like to support WordPress, having the "powered by" link someone on your blog is the best way, it's our only promotion or advertising. -->
 	<p>
 		<?php bloginfo('name'); ?> is proudly powered by
-		<a href="http://mu.wordpress.org/">WordPress MU</a> running on <a href="http://<?php echo $current_site->domain . $current_site->path ?>"><?php echo $current_site->site_name ?></a>. <a href="http://<?php echo $current_site->domain . $current_site->path ?>wp-signup.php" title="Create a new blog">Create a new blog</a> and join in the fun!
+		<a href="http://mu.wordpress.org/">WordPress MU</a> provided by <a href="http://<?php echo $current_site->domain . $current_site->path ?>"><?php echo $current_site->site_name ?></a>. 
 		<br /><a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a>
 		and <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>.
 		<!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
