b4d90a0afe6a59fe3c945523c791932c8c14a82f
bse <bse@committer.creativecommons.org>
2007-08-21 21:44:09 +0000

    Bad Blog tab! Don't activate on home page!

--- cc4/nav.php
+++ cc4/nav.php
@@ -27,7 +27,7 @@ $nav = array (
           
             // check if we're in the ccInternational area
             if (in_category(21) and $item["name"] == "International") $klass = "active";
-            if (in_category(1) and $item["name"] == "Blog") $klass = "active";
+            if (in_category(1) and $item["name"] == "Blog" and !is_home()) $klass = "active";
           }
           
           print '      <li class="'.$klass.'"><a href="'.$item["path"].'" title="'.$item["name"].'">'.$item["name"].'</a></li>';
