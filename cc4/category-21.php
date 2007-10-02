99cb2535536ce8886fd1f43abc32469313d5c9d2
bse <bse@committer.creativecommons.org>
2007-10-02 16:56:00 +0000

    Updated intro text.

--- cc4/category-21.php
+++ cc4/category-21.php
@@ -35,8 +35,13 @@ $is_international = true;
         	</div>
           <div id="alpha" class="content-box">
            <div class="block page">
-            <p>We are excited to announce Creative Commons International &mdash; an offshoot of our licensing project dedicated to the drafting and eventual adoption of jurisdiction-specific licenses. Creative Commons International is being lead by <a href="http://creativecommons.org/about/people#65">Catharina Maracke</a> (<a href="mailto:catharina@creativecommons.org">email</a>), with help from member jurisdictions.</p>
-            <h3>Completed Licenses</h3>
+            
+<p>Creative Commons International (CCi) works to "port" the core Creative Commons Licenses to different copyright legislations around the world. The porting process involves both linguistically translating the licenses and legally adapting them to particular jurisdictions.</p>
+
+<p>This work is lead by CCi director <a href="http://creativecommons.org/about/people#65">Catharina Maracke</a> (<a href="mailto:catharina@creativecommons.org">email</a>) and volunteer teams in each jurisdiction who are committed to introducing CC to their country and who consult extensively with members of the public and key stakeholders in an effort to adapt the CC licenses to their jurisdiction.</p>
+
+<p>A complete overview of the porting process can be found <a href="http://wiki.creativecommons.org/Worldwide_Overview">here</a>.</p>
+<h3>Completed Licenses</h3>
             <p>We have completed the process and developed licenses for the following jurisdictions:</p>
 <?php 
     query_posts("cat=21&orderby=title&order=ASC&posts_per_page=-1");
