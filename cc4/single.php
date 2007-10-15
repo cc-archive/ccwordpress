bbc4ed898effe23f017329d9fa7c587477b026f7
bse <bse@committer.creativecommons.org>
2007-10-15 21:46:46 +0000

    proper linkbacks to category archives.

--- cc4/single.php
+++ cc4/single.php
@@ -40,7 +40,7 @@ if (have_posts())  {
 <? if ($is_commoner) {?>
 						<h3 class="category">
 						  <? $cat = get_the_category(); (count($cat) > 1) ? $cat = $cat[1] : $cat = $cat[0]; ?>
-							<a href="<?php echo get_category_link ($cat->cat_ID); ?>">
+							<a href="<?php echo get_option('home') . "/" . $cat->category_nicename . "/"; ?>">
 								<? echo $cat->cat_name; ?>
 							</a>
 						</h3>
