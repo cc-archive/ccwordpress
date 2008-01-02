6aba1547acab38694b2bda3d4a50a4a0d1afcdff
bse <bse@committer.creativecommons.org>
2008-01-02 22:28:55 +0000

    Search form styling

--- www/wp-content/themes/cc4/searchform.php
+++ www/wp-content/themes/cc4/searchform.php
@@ -1,7 +1,9 @@
 <form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
-<div><input type="text" value="<?php the_search_query(); ?>" name="s" id="s" size="30" />
+<div>
+<p>
+<input type="text" value="<?php the_search_query(); ?>" name="s" id="s" size="30" />
 <input type="submit" id="searchsubmit" value="Search" />
-&nbsp;&nbsp;&nbsp;
+</p>
 <label><input type="radio" name="st" value="blog" checked /> Search blog</label>
 &nbsp;&nbsp;&nbsp;
 <label><input type="radio" name="st" value="site" /> Search creativecommons.org</label>
