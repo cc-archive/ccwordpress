a5ad45a5d1cbc24b5b91b5d8a27ec5ac3412f7d7
bse <bse@committer.creativecommons.org>
2008-01-02 20:26:51 +0000

    Slightly more fitting search templates. Should probably show some post preview. Also need to make "Search site" option redirect to google.

--- www/wp-content/themes/cc4/search.php
+++ www/wp-content/themes/cc4/search.php
@@ -1,42 +1,38 @@
 <?php get_header(); ?>
+    <div id="body">
+      <div id="splash">
 
-	<div id="content" class="narrowcolumn">
+        <div id="splash-menu">
+          
+        </div>
+      </div>
 
-	<?php if (have_posts()) : ?>
+      <div id="content">
+        <div id="main-content">
+          <div class="block" id="title">
+        <h2>
+          Search Results
+        </h2>
+          </div>
+          <div id="alpha" class="content-box">
+           <div class="block hero">
+          		<?php include (TEMPLATEPATH . '/searchform.php'); ?>
+           </div>
+          
+<?php if (have_posts())  { ?>
+<?php while (have_posts()) { the_post(); ?>
 
-		<h2 class="pagetitle">Search Results</h2>
-
-		<div class="navigation">
-			<div class="alignleft"><?php next_posts_link('&laquo; Older Entries') ?></div>
-			<div class="alignright"><?php previous_posts_link('Newer Entries &raquo;') ?></div>
-		</div>
-
-
-		<?php while (have_posts()) : the_post(); ?>
-
-			<div class="post">
-				<h3 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
+			<div class="block blogged">
+				<h3 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h3>
 				<small><?php the_time('l, F jS, Y') ?></small>
 
 				<p class="postmetadata"><?php the_tags('Tags: ', ', ', '<br />'); ?> Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' | '); ?>  <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?></p>
 			</div>
 
-		<?php endwhile; ?>
 
-		<div class="navigation">
-			<div class="alignleft"><?php next_posts_link('&laquo; Older Entries') ?></div>
-			<div class="alignright"><?php previous_posts_link('Newer Entries &raquo;') ?></div>
-		</div>
-
-	<?php else : ?>
-
-		<h2 class="center">No posts found. Try a different search?</h2>
-		<?php include (TEMPLATEPATH . '/searchform.php'); ?>
-
-	<?php endif; ?>
-
-	</div>
-
+<?php } }?>
+            <?php posts_nav_link(' &mdash; ', 'previous page', 'next page'); ?>
+          </div>
+        </div>  
 <?php get_sidebar(); ?>
-
 <?php get_footer(); ?>
--- www/wp-content/themes/cc4/searchform.php
+++ www/wp-content/themes/cc4/searchform.php
@@ -1,5 +1,9 @@
 <form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
-<div><input type="text" value="<?php the_search_query(); ?>" name="s" id="s" />
+<div><input type="text" value="<?php the_search_query(); ?>" name="s" id="s" size="30" />
 <input type="submit" id="searchsubmit" value="Search" />
+&nbsp;&nbsp;&nbsp;
+<label><input type="radio" name="searchtype" value="blog" checked /> Search blog</label>
+&nbsp;&nbsp;&nbsp;
+<label><input type="radio" name="searchtype" value="site" /> Search site</label>
 </div>
 </form>
