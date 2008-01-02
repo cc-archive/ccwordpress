7f5fb7d4fd56c5a1c7f779a017071fbe0d339467
bse <bse@committer.creativecommons.org>
2008-01-02 22:59:57 +0000

    Anotehr fix to WP retardedness - "<label><br />" is so not a good thing, wtf? cc_post_content_process() strips these now.

--- www/wp-content/themes/cc4/functions.php
+++ www/wp-content/themes/cc4/functions.php
@@ -90,8 +90,8 @@ function cc_current_feature() {
 // Removes over-zealously placed <br/>'s and </p>'s from commented out html, and RDF blocks
 // Also removes <br/>'s from the end of </li>'s
 function cc_post_content_process($content) {
-  $wrong_br = array("/(<\!--.*)?<(br\/|\/p)>/", "/(<\!--.*)?<p>\&\#8211;>/", "/<\/li><br\s+\/>/");
-  $fixed = array("$1", "$1 -->", "</li>");
+  $wrong_br = array("/(<\!--.*)?<(br\/|\/p)>/", "/(<\!--.*)?<p>\&\#8211;>/", "/<\/li><br\s+\/>/", "/<label><br\s+\/>/");
+  $fixed = array("$1", "$1 -->", "</li>", "<label>");
   
   $content = preg_replace($wrong_br, $fixed, $content);
 
