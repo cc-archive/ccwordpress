845e72ccc69299152b8a8da1e19c7891ba30058f
bse <bse@committer.creativecommons.org>
2008-01-03 00:17:53 +0000

    Footer based search! Found a last minute bug.. gotta fix first...

--- www/wp-content/themes/cc4/searchform.php
+++ www/wp-content/themes/cc4/searchform.php
@@ -1,10 +1,10 @@
 <form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
 <div>
-<p>
+<? if (!$is_footer) {?><p><? } ?>
 <input type="text" value="<?php the_search_query(); ?>" name="s" id="s" size="30" />
 <input type="submit" id="searchsubmit" value="Search" />
-</p>
-<? if (!is_category()) { ?>
+<? if (!$is_footer) {?></p><? } ?>
+<? if (!is_category() && !$is_footer) { ?>
 <label><input type="radio" name="st" value="blog" checked /> Search blog</label>
 &nbsp;&nbsp;&nbsp;
 <label><input type="radio" name="st" value="site" /> Search creativecommons.org using Google</label>
