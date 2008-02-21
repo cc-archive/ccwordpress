5aea205493bb5195f8b08062297b4e6cb9d93cf2
nyergler <nyergler@committer.creativecommons.org>
2008-02-21 23:44:26 +0000

    Merged revisions 9146 via svnmerge from https://cctools.svn.sourceforge.net/svnroot/cctools/ccwordpress/trunk
    ........
      r9146 | nyergler | 2008-02-21 15:30:30 -0800 (Thu, 21 Feb 2008) | 2 lines
      
      Updated javascript to YUI 2.5, modal popups.
    ........

--- www/includes/help.js
+++ www/includes/help.js
@@ -13,32 +13,29 @@ YAHOO.cc.help.init_help_item = function(help_anchor) {
 
     // create the new panel and position it
     var new_panel = new YAHOO.widget.Panel(help_id, 
-{close: false, visible: false, draggable: false, width:250,
- effect:{effect:YAHOO.widget.ContainerEffect.FADE,duration:0.35} } ); 
-
-    var link_region = YAHOO.util.Dom.getRegion(link_id);
-    if (link_region) {
-       new_panel.cfg.setProperty('xy',
-		[link_region.right + 5, link_region.top] );
-    }
+                            {close: true, 
+			     visible: false, 
+			     draggable: false, 
+			     width:250,
+			     context:[help_anchor.id,'bl','tl']
+			    } ); 
 
     var item_idx = YAHOO.cc.help.help_panels.push(new_panel) - 1;
 
     YAHOO.cc.help.help_panels[item_idx].render();
 
     // connect the event handlers
-    YAHOO.util.Event.addListener(link_id, "mouseover", 
-				 function(e) {YAHOO.cc.help.help_panels[item_idx].show();});
-    YAHOO.util.Event.addListener(link_id, "mouseout", 
-    			function(e) {window.setTimeout("YAHOO.cc.help.help_panels[" + item_idx + "].hide();", 1000);});
+    YAHOO.util.Event.addListener(link_id, "click", 
+				 function(e) {
+				   YAHOO.cc.help.help_panels[item_idx].show();
+				   e.preventDefault();
+				 });
 
-    // disable clicks
-    YAHOO.util.Event.addListener(link_id, 'click', function(e){YAHOO.util.Event.preventDefault(e);});
 
 } // init_help_text
 
 YAHOO.cc.help.init = function() {
-    // initialization for help roll-overs (tooltips on steroids)
+    // initialization for help pop-ups
 
     YAHOO.util.Dom.getElementsByClassName('helpLink', 'a', 'body',
 				     YAHOO.cc.help.init_help_item);
