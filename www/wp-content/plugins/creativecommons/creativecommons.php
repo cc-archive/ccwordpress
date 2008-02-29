7742152e3c168fab48647e55f797d1b38f1a2799
bse <bse@committer.creativecommons.org>
2008-02-29 20:41:24 +0000

    Merged revisions 9248 via svnmerge from https://cctools.svn.sourceforge.net/svnroot/cctools/ccwordpress/trunk
    ........
      r9248 | bse | 2008-02-29 14:36:39 -0600 (Fri, 29 Feb 2008) | 2 lines
      
      Force the use of WP's built-in version of Magpie
    ........

--- www/wp-content/plugins/creativecommons/creativecommons.php
+++ www/wp-content/plugins/creativecommons/creativecommons.php
@@ -10,9 +10,11 @@ Author URI:
 
 require_once "creativecommons-admin.php";
 
+/* Use WP built-in version of magpie. - 29/2/08, ar */
+require_once (ABSPATH . WPINC . '/rss.php');
+
 /* As seen in http://freeculture.org:8080/svn/wordpress-theme/trunk/front_page/feeds_chapter.php */
 function cc_build_external_feed($feedid = 'Planet CC', $singlecat = false, $showcat = true, $entries = 8, $charcount = 300) {
-//  require_once "magpie/rss_fetch.inc";
   global $cc_db_rss_table;
   global $wpdb;
   
