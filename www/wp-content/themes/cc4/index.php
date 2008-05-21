7450efd1aab2e574b74977297bee8678efdbe3ac
nkinkade <nkinkade@committer.creativecommons.org>
2008-05-21 18:04:32 +0000

    Committing a number of outstanding local changes .. have no idea what they really are.

--- www/wp-content/themes/cc4/index.php
+++ www/wp-content/themes/cc4/index.php
@@ -5,22 +5,6 @@
       <div id="content">
         <div id="main-content">
 <?php /* removing splash featured things for now - ar, 2008-04-25 
-	<div id="splash">
-	 <div class="splash-box">
-		<div class="splash-box-left">
-			<a href="http://creativecommons.org/weblog/entry/8051">
-				<img src="/images/splash/left-fcw.png" alt="Free Cultural Works" border="0" />
-			</a>
-			<p class="info">A seal marking CC licenses that qualify as Free Culture Licenses according to the Definition of Free Cultural Works.</p>
-		</div>
-		<div class="splash-box-right">
-			<a href="http://creativecommons.org/weblog/entry/8091">
-				<img src="/images/splash/right-pcc.png" alt="Planet Creative Commons" border="0" />
-			</a>
-                        <p class="info" >Aggregating blogs from Creative Commons, CC jurisdiction projects, and the CC community.</p>
-		</div>
-	 </div>
-	</div>
 */ ?>
 	
 	<div class="block hero" id="title">
