56b95c57171c062dc8eaa2e02ebca42ff5e7ffa6
bse <bse@committer.creativecommons.org>
2008-06-24 22:49:57 +0000

    revised-revision. Press Releases in.

--- www/wp-content/themes/cc4/sidebar.php
+++ www/wp-content/themes/cc4/sidebar.php
@@ -23,30 +23,26 @@
       <li><a href="/presskit">Press Kit</a></li>
     </ul>
   </div>
-
   <div class="sideitem">
     <ul>
-      <li ><a href="/commoners" >Commoners</a></li>
+      <li ><a href="/commoners" >Creative Commoners</a></li>
+      <li><a href="/press-releases">Press Releaes</a></li>
+<?php /*
       <li ><a href="/audio" >Audio</a></li>
       <li ><a href="/video" >Video</a></li>
       <li ><a href="/image" >Images</a></li>
       <li ><a href="/text" >Text</a></li>
       <li ><a href="/software" >Software</a></li>
+*/ ?>
     </ul>
   </div>  
-  
+
   <div class="sideitem">
     <h4>The Commons</h4>
     <ul>
       <li><img src="/images/commons/sc.png"> <a href="http://sciencecommons.org/">Science Commons</a></li>
       <li><img src="/images/commons/cci.png"> <a href="http://creativecommons.org/international/">ccInternational</a></li>
       <li><img src="/images/commons/learn.png"> <a href="http://learn.creativecommons.org/">ccLearn</a></li>
-    </ul>
-  </div>
-
-  <div class="sideitem">
-    <ul>
-      <li ><a href="http://icommonssummit.org" >iSummit 2008</a></li>
     </ul>
   </div>
 
