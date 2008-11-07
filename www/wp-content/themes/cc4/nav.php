7b54061bcf713b1848e8f09465d9178b8328b618
bse <bse@committer.creativecommons.org>
2008-11-07 00:33:08 +0000

    Revised tab nav.

--- www/wp-content/themes/cc4/nav.php
+++ www/wp-content/themes/cc4/nav.php
@@ -3,10 +3,11 @@
 $nav = array (
   array("name" => "Home",           "path" => get_option('home')),
   array("name" => "About",          "path" => "/about/"),
-  array("name" => "Support",        "path" => "http://support.creativecommons.org"),
-/*  array("name" => "License",      "path" => "/license/"),*/
+  array("name" => "FAQ",			"path" => "http://wiki.creativecommons.org/FFAQ"),
+  array("name" => "Donate",        "path" => "http://support.creativecommons.org"),
+  array("name" => "Wiki",    "path" => "http://wiki.creativecommons.org/"),
   array("name" => "Projects",       "path" => "/projects/"),
-  array("name" => "Participate",    "path" => "http://wiki.creativecommons.org/"),
+/*  array("name" => "License",      "path" => "/license/"),*/
   array("name" => "International",  "path" => "/international/"),
   array("name" => "Contact",        "path" => "/contact/"),
 );
@@ -33,7 +34,7 @@ $nav = array (
   //          if ((is_category(1) or (in_category(1) and is_single())) and $item["name"] == "Blog" and !is_home() and !is_page()) $klass = "active";
           }
           
-          print '      <li class="'.$klass.'"><a href="'.$item["path"].'" title="'.$item["name"].'"><span>'.$item["name"].'</span></a></li>';
+          print '      <li class="'.strtolower($item["name"]) . ' ' . $klass.'"><a href="'.$item["path"].'" title="'.$item["name"].'"><span>'.$item["name"].'</span></a></li>';
         } 
         
         if (is_page() or is_single()) edit_post_link('<span>Edit this article</span>', '<li class="edit">', '</li>');
@@ -41,6 +42,5 @@ $nav = array (
         ?>
         
     </ul>
-    <div class="clear"></div>
   </div>
  
