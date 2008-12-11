dc7ae90d9a8f535f8686f29e49c9fad47b212db5
bse <bse@committer.creativecommons.org>
2008-12-11 23:04:37 +0000

    Merged revisions 11409 via svnmerge from svn+ssh://svn@code.creativecommons.org/svnroot/ccwordpress/trunk
    ........
      r11409 | bse | 2008-12-11 23:03:24 +0000 (Thu, 11 Dec 2008) | 2 lines
      
      Revised sidebar, with contextual efforts in the sections that need it - About/
    ........

--- www/wp-content/themes/cc4/sidebar.php
+++ www/wp-content/themes/cc4/sidebar.php
@@ -13,22 +13,36 @@
     <?php $is_sidebar = true; include (TEMPLATEPATH . '/searchform.php'); ?>
     <div class="clear"></div>
   </div>
-  
+
+<?php if (!is_home()) { 
+	$exclude_list = "7486,7476,7471,7472,7473,7506,7474,7475,7487,7505,7682,7793,7794";
+	$list_pages_query = "&title_li=&echo=0&exclude=".$exclude_list;
+	if ($post->post_parent) {
+		if ($root_post_id = get_post_meta($post->ID, "root", true)) {
+			$child_id = $root_post_id;
+		} else {
+			$child_id = $post->post_parent;
+		}
+	} else {
+		$child_id = $post->ID;
+	}
+	
+	if ($child_id) { $pages_list = wp_list_pages('child_of='.$child_id.$list_pages_query); }
+
+    if ($pages_list) {
+		$pages_list = "<div class=\"sideitem\"><ul>" . $pages_list . "</ul></div>";
+		echo $pages_list;
+	}
+ } ?>
   <div class="sideitem">
     <ul>
       <li ><a href="/commoners" >Creative Commoners</a></li>
       <li><a href="/projects/documentation">Documentation</a></li>
       <li><a href="/projects/casestudies">Case Studies</a></li>
-      <li><a href="http://wiki.creativecommons.org/FFAQ">FAQ</a></li>
-      <li><a href="/about/opportunities">Opportunities</a></li>
-      <li><a href="/presskit">Press Kit</a></li>
-    </ul>
-  </div>
-  <div class="sideitem">
-    <ul>
-      <li><a href="/press-releases">Press Releases</a></li>
-      <li><a href="/about/newsletter">Newsletter</a></li>
+      <!-- li><a href="/about/opportunities">Opportunities</a></li-->
       <li><a href="http://wiki.creativecommons.org/Events">Events</a></li>
+      <li><a href="/about/newsletter">Newsletter</a></li>
+      <li><a href="/about/press">Press Room</a></li>
     </ul>
   </div>
 
