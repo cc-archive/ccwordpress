857358c8da83f5494193d13347fdd3d47a0f460e
nkinkade <nkinkade@committer.creativecommons.org>
2009-07-15 19:30:56 +0000

    Changed FAQ links from bogus redirected ones that went to wiki/FAQ, to go directly to wiki/FFAQ.

--- www/wp-content/themes/cc5/nav.php
+++ www/wp-content/themes/cc5/nav.php
@@ -3,7 +3,7 @@
     <!-- <li><em>Home</em></li> -->
     <li><a href="/about/">About</a></li>
     <li><a href="<?php echo get_category_link(1); ?>">News</a></li>
-    <li><a href="/faq">FAQ</a></li>
+    <li><a href="http://wiki.creativecommons.org/FFAQ">FAQ</a></li>
     <li><a href="http://support.creativecommons.org/">Donate</a></li>
     <li><a href="http://wiki.creativecommons.org/">Wiki</a></li>
     <li><a href="/projects/">Projects</a></li>
