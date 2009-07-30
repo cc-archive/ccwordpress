ee78163ef1bae42913b355cc142086ea6890f565
akozak <akozak@committer.creativecommons.org>
2009-07-30 21:55:49 +0000

    Totally remove events map

--- www/wp-content/themes/cclearn/index.php
+++ www/wp-content/themes/cclearn/index.php
@@ -10,7 +10,6 @@
        <!-- <div id="splash">
 		-->	<?php /*echo cc_current_feature(); */?>
 		
-		/* <?php include "events-map.php" ?> */
 		<div id="events-map">
 		<img src="http://learn.creativecommons.org/wp-content/uploads/2009/07/opened-launch.png" />
 		</div>
