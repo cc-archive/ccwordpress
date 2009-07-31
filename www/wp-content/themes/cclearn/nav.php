75e4ce3f3b33f5ce44184b75b59efeb5591a8784
akozak <akozak@committer.creativecommons.org>
2009-07-31 16:48:42 +0000

    Moved divs outside the navbar links

--- www/wp-content/themes/cclearn/nav.php
+++ www/wp-content/themes/cclearn/nav.php
@@ -39,7 +39,7 @@ $nav = array (
 			if ("active" == $klass) $activated = true;
           }
 
-          print '      <li class="'.$klass.'"><a href="'. get_option('home') . $item["path"] .'" title="'.$item["name"].'"><div>'.$item["name"].'</div></a></li>';
+          print '      <li class="'.$klass.'"><div><a href="'. get_option('home') . $item["path"] .'" title="'.$item["name"].'">'.$item["name"].'</a></div></li>';
         } 
         
         if (is_page() or is_single()) edit_post_link('<span>Edit this article</span>', '<li class="edit">', '</li>');
