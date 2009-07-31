4c8a84d504611e71e1da08c631a82146a8ad04e9
akozak <akozak@committer.creativecommons.org>
2009-07-31 16:51:53 +0000

    Move around edit button to test its div

--- www/wp-content/themes/cclearn/nav.php
+++ www/wp-content/themes/cclearn/nav.php
@@ -42,7 +42,7 @@ $nav = array (
           print '      <li class="'.$klass.'"><div><a href="'. get_option('home') . $item["path"] .'" title="'.$item["name"].'">'.$item["name"].'</a></div></li>';
         } 
         
-        if (is_page() or is_single()) edit_post_link('<span>Edit this article</span>', '<li class="edit">', '</li>');
+        if (is_page() or is_single()) edit_post_link('<div class="edit">Edit this article</div>', '<li class="edit">', '</li>');
        
         ?>
         
