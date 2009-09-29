052a8366fdcd525175550c3e4a8bf1a9da664f22
bse <bse@committer.creativecommons.org>
2009-09-29 21:06:13 +0000

    Merged revisions 13445-13446 via svnmerge from svn+ssh://svn@code.creativecommons.org/svnroot/ccwordpress/trunk
    ........
      r13445 | bse | 2009-09-29 21:01:54 +0000 (Tue, 29 Sep 2009) | 2 lines
      
      Added Developers page link.
    ........
      r13446 | bse | 2009-09-29 21:05:35 +0000 (Tue, 29 Sep 2009) | 1 line
      
      Fixed nested ul's in the sidebar.
    ........

--- www/wp-content/themes/cc5/sidelinks.php
+++ www/wp-content/themes/cc5/sidelinks.php
@@ -1,8 +1,9 @@
 <!-- <li><a href="#">About CC</a></li> -->
+<li><a href="http://support.creativecommons.org/store/">Store</a></li>
+<li><a href="http://wiki.creativecommons.org/Developers">Developers</a></li>
 <li><a href="/about/opportunities">Jobs</a></li>
 <li><a href="/about/events">Events</a></li>
 <li><a href="/about/newsletter">Newsletter</a></li>
 <li><a href="http://wiki.creativecommons.org/Case_Studies">Case Studies</a></li>
 <li><a href="/interviews">CC Talks With...</a></li>
-<li><a href="http://support.creativecommons.org/store/">Store</a></li>
 <!-- <li><a href="#">Documentation</a></li> -->
