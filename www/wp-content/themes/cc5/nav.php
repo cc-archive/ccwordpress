141db1e1061fcc9dd802b4670b96562c54c411ad
bse <bse@committer.creativecommons.org>
2009-10-22 20:42:07 +0000

    Added Store link to main navbar; replaced old Store link with link to the Licenses about page

--- www/wp-content/themes/cc5/nav.php
+++ www/wp-content/themes/cc5/nav.php
@@ -3,10 +3,11 @@
     <!-- <li><em>Home</em></li> -->
     <li><a href="/about/">About</a></li>
     <li><a href="<?php echo get_category_link(1); ?>">News</a></li>
-    <li><a href="http://wiki.creativecommons.org/FFAQ">FAQ</a></li>
     <li><a href="http://support.creativecommons.org/">Donate</a></li>
+    <li><a href="http://wiki.creativecommons.org/FFAQ">FAQ</a></li>
     <li><a href="http://wiki.creativecommons.org/">Wiki</a></li>
     <li><a href="/projects/">Projects</a></li>
+    <li><a href="http://support.creativecommons.org/store">Store</a></li>
     <li class="helpLink" id="international_list"><a href="/international/">International</a></li>
   </ul>
 </div>
