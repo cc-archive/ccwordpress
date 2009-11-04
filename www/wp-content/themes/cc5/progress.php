e5e8d6ffc8d31f7646141deb19b94f30c6fe4e5a
bse <bse@committer.creativecommons.org>
2009-11-04 23:35:27 +0000

    Merged revisions 13632 via svnmerge from svn+ssh://svn@code.creativecommons.org/svnroot/ccwordpress/trunk
    ........
      r13632 | bse | 2009-11-04 23:34:51 +0000 (Wed, 04 Nov 2009) | 1 line
      
      Made thermometer goal a link -- based on observations of user click patterns, people expect this to be a link.
    ........

--- www/wp-content/themes/cc5/progress.php
+++ www/wp-content/themes/cc5/progress.php
@@ -3,7 +3,7 @@
 				<div class="progress <?php if (is_home()) {?>home<?}?>" onclick="window.location='https://support.creativecommons.org/donate';">
 					<span>&nbsp;</span>
 				</div>
-				<?php if (is_home()) { ?><div class="homeGoal">$500,000</div><? } ?>
+				<?php if (is_home()) { ?><div class="homeGoal"><a href="https://support.creativecommons.org/donate">$500,000</a></div><? } ?>
 				<div class="results<?php if (is_home()) {?>Home<?}?>">
 					<a href="https://support.creativecommons.org/donate">
 					<?php if (is_home()) { ?><strong><?php cc_progress_total() ?> Raised</strong> &mdash; Help us reach our goal by Dec 31!<?php } else { ?>
