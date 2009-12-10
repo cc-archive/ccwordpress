9a9e7d5f974ef300c4271f29284f672d1d2bbdde
bse <bse@committer.creativecommons.org>
2009-12-10 19:10:35 +0000

    Merged revisions 13774 via svnmerge from svn+ssh://svn@code.creativecommons.org/svnroot/ccwordpress/trunk
    ........
      r13774 | bse | 2009-12-10 19:09:30 +0000 (Thu, 10 Dec 2009) | 1 line
      
      Added Supporters link to sidelinks
    ........

--- www/wp-content/themes/cc5/sidelinks.php
+++ www/wp-content/themes/cc5/sidelinks.php
@@ -1,9 +1,10 @@
 <!-- <li><a href="#">About CC</a></li> -->
 <li><a href="/about/licenses/">The Licenses</a></li>
 <li><a href="http://wiki.creativecommons.org/Developers">Developers</a></li>
-<li><a href="/about/opportunities">Jobs</a></li>
+<li><a href="https://support.creativecommons.org/supporters">Supporters</a></li>
 <li><a href="/about/events">Events</a></li>
 <li><a href="/about/newsletter">Newsletter</a></li>
+<li><a href="/about/opportunities">Jobs</a></li>
 <li><a href="http://wiki.creativecommons.org/Case_Studies">Case Studies</a></li>
 <li><a href="/interviews">CC Talks With...</a></li>
 <li><a href="/contact">Contact</a></li>
