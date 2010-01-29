3c085d3db952cf012d69af58841b31c19cbd8513
bse <bse@committer.creativecommons.org>
2010-01-29 19:34:25 +0000

    Merged revisions 14016 via svnmerge from svn+ssh://svn@code.creativecommons.org/svnroot/ccwordpress/trunk
    ........
      r14016 | bse | 2010-01-29 19:24:55 +0000 (Fri, 29 Jan 2010) | 1 line
      
      Removed 'Programs' column from homepage, rearranged links into two columns
    ........

--- www/wp-content/themes/cc5/sidelinks.php
+++ www/wp-content/themes/cc5/sidelinks.php
@@ -1,11 +1,13 @@
 <!-- <li><a href="#">About CC</a></li> -->
 <li><a href="/about/licenses/">The Licenses</a></li>
-<li><a href="http://wiki.creativecommons.org/Developers">Developers</a></li>
-<li><a href="https://support.creativecommons.org/supporters">Supporters</a></li>
-<li><a href="/about/events">Events</a></li>
 <li><a href="/about/newsletter">Newsletter</a></li>
+<li><a href="/about/events">Events</a></li>
+<li><a href="https://support.creativecommons.org/supporters">Supporters</a></li>
 <li><a href="/about/opportunities">Jobs</a></li>
+<li><a href="http://wiki.creativecommons.org/Developers">Developers</a></li>
 <li><a href="http://wiki.creativecommons.org/Case_Studies">Case Studies</a></li>
 <li><a href="/interviews">CC Talks With...</a></li>
+<li><a href="http://learn.creativecommons.org/">Education</a></li>
+<li><a href="http://sciencecommons.org/">Science</a></li>
 <li><a href="/contact">Contact</a></li>
 <!-- <li><a href="#">Documentation</a></li> -->
