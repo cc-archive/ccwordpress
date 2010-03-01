e20840bf412b6089b0e2d380fe77006175eed4d0
leo_arias <leo_arias@committer.creativecommons.org>
2010-03-01 22:53:41 +0000

    Added i18n functions to category-7.php and category-21.php. Added the strings from those files to pot and traslated those strings to Spanish.

--- www/wp-content/themes/cc5/category-21.php
+++ www/wp-content/themes/cc5/category-21.php
@@ -28,19 +28,19 @@ $is_international = true;
     <div id="mainContent" class="box">
       <div id="contentPrimary">
         	<div class="block" id="title">
-        		<h2>International</h2>
+        		<h2><?php _e('International', 'cc5'); ?></h2>
         	</div>
         	<div id="blocks">
             <div class="block page sideContentSpace">
 
-				<p>Creative Commons works to "port" the core Creative Commons Licenses to different copyright legislations around the world. The porting process involves both linguistically translating the licenses and legally adapting them to particular jurisdictions.</p>
+				<p><?php _e('Creative Commons works to "port" the core Creative Commons Licenses to different copyright legislations around the world. The porting process involves both linguistically translating the licenses and legally adapting them to particular jurisdictions.', 'cc5'); ?></p>
 
-				<p>This work is led by CC General Counsel <a href="http://creativecommons.org/about/people#95">Diane Peters</a> (<a href="mailto:diane@creativecommons.org">email</a>) with Project Manager <a href="http://creativecommons.org/about/people#85">Michelle Thorne</a> (<a href="mailto:michelle@creativecommons.org">email</a>) and volunteer teams in each jurisdiction who are committed to introducing CC to their country and who consult extensively with members of the public and key stakeholders in an effort to adapt the CC licenses to their jurisdiction.</p>
+				<p><?php _e('This work is led by CC General Counsel <a href="http://creativecommons.org/about/people#95">Diane Peters</a> (<a href="mailto:diane@creativecommons.org">email</a>) with Project Manager <a href="http://creativecommons.org/about/people#85">Michelle Thorne</a> (<a href="mailto:michelle@creativecommons.org">email</a>) and volunteer teams in each jurisdiction who are committed to introducing CC to their country and who consult extensively with members of the public and key stakeholders in an effort to adapt the CC licenses to their jurisdiction.', 'cc5'); ?></p>
 
-				<p>A complete overview of the porting process can be found at <a href="http://wiki.creativecommons.org/International_Overview">http://wiki.creativecommons.org/International_Overview</a>.</p>
+				<p><?php _e('A complete overview of the porting process can be found at <a href="http://wiki.creativecommons.org/International_Overview">http://wiki.creativecommons.org/International_Overview</a>.', 'cc5'); ?></p>
 
-              <h3>Completed Licenses</h3>
-              <p>We have completed the process and developed licenses for the following jurisdictions:</p>
+              <h3><?php _e('Completed Licenses', 'cc5'); ?></h3>
+              <p><?php _e('We have completed the process and developed licenses for the following jurisdictions:', 'cc5'); ?></p>
 <?php 
     query_posts("cat=21&orderby=title&order=ASC&posts_per_page=-1");
     if (have_posts())  { ?>
@@ -63,9 +63,9 @@ $is_international = true;
               </div>
               <br clear="all" />
 
-              <h3>Project Jurisdictions</h3>
+              <h3><?php _e('Project Jurisdictions', 'cc5'); ?></h3>
 
-              <p>The process of developing licenses and discussing them are still in progress for the following jurisdictions:</p>
+              <p><?php _e('The process of developing licenses and discussing them are still in progress for the following jurisdictions:', 'cc5'); ?></p>
 
               <div class="icontainer">
 <?php rewind_posts(); while (have_posts()) { the_post();
@@ -86,7 +86,7 @@ $is_international = true;
           </div>
 
           <div id="sideContent">
-            <h4>Upcoming Project Jurisdictions</h4>
+            <h4><?php _e('Upcoming Project Jurisdictions', 'cc5'); ?></h4>
             <ul>
 <?php rewind_posts(); while (have_posts()) { the_post();
     $jurisdiction = get_the_jurisdiction($post->ID);
