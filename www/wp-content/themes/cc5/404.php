676eb892907069a020584573eb1c4a5e37aae0e0
leo_arias <leo_arias@committer.creativecommons.org>
2010-03-01 21:55:22 +0000

    Added internationalization functions to 404.php. Added the function to load the traslation in header.php. Updated the pot to include 404.php strings. Translated 404.php strings to Spanish.

--- www/wp-content/themes/cc5/404.php
+++ www/wp-content/themes/cc5/404.php
@@ -2,15 +2,15 @@
 
 <div id="mainContent" class="box">
   <div id="contentPrimary">
-          <div class="block" id="title"><h2>Sorry...</h2></div>
+          <div class="block" id="title"><h2><?php _e('Sorry...', 'cc5'); ?></h2></div>
             <div class="block">
-              <p>The page you were looking for was not found.<br/>&nbsp;</p>
-              <h4>Other Content</h4>
+              <p><?php _e('The page you were looking for was not found.', 'cc5'); ?><br/>&nbsp;</p>
+              <h4><?php _e('Other Content', 'cc5'); ?></h4>
               <ul class="archives">
-                <li><a href="<?php echo get_settings('home'); ?>">Home</a></li>
-                <li><a href="<?php echo get_settings('home'); ?>/weblog/">Weblog</a></li>
-                <li><a href="http://wiki.creativecommons.org">Wiki</a></li>
-                <li><a href="http://search.creativecommons.org">ccSearch</a></li>
+                <li><a href="<?php echo get_settings('home'); ?>"><?php _e('Home', 'cc5'); ?></a></li>
+                <li><a href="<?php echo get_settings('home'); ?>/weblog/"><?php _e('Weblog', 'cc5'); ?></a></li>
+                <li><a href="http://wiki.creativecommons.org"><?php _e('Wiki', 'cc5'); ?></a></li>
+                <li><a href="http://search.creativecommons.org"><?php _e('ccSearch', 'cc5'); ?></a></li>
               </ul>
           </div>
         </div>
--- www/wp-content/themes/cc5/header.php
+++ www/wp-content/themes/cc5/header.php
@@ -12,6 +12,8 @@
 //	$showTestimonial = rand(0, 1);
 // 	setcookie('cc_showtestimonial', $showTestimonial, time() + 60 * 60 * 24 * 30);
 // }
+
+load_plugin_textdomain('cc5');
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
