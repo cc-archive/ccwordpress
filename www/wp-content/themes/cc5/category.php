c9437921b3c8431e7ac8804690c566cfb8304036
leo_arias <leo_arias@committer.creativecommons.org>
2010-03-01 22:59:05 +0000

    Added i18n functions to category.php

--- www/wp-content/themes/cc5/category.php
+++ www/wp-content/themes/cc5/category.php
@@ -23,13 +23,13 @@ $category = get_category($cat); ?>
               <div class="block blog sideContentSpace" id="post-<?php the_ID(); ?>">
                 <h1 class="title">
                   <a href="<? the_permalink() ?>">
-                   <?php if (in_category(4) || in_category(7)) { ?>CC Talks With: <?php } ?> 
+                   <?php if (in_category(4) || in_category(7)) { _e('CC Talks With: ', 'cc5'); } ?> 
                    <?php the_title() ?>
                   </a>
                 </h1>
                 <h4 class="meta"><?php the_author() ?>, <?php the_time('F jS, Y')?></h4>
-                <?php the_content("Read More..."); ?>
-                <?php edit_post_link('Edit', '', ' |'); ?> <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?> 
+                <?php the_content(__('Read More...', 'cc5')); ?>
+                <?php edit_post_link(__('Edit', 'cc5'), '', ' |'); ?> <?php comments_popup_link(__('No Comments &#187;', 'cc5'), __('1 Comment &#187;', 'cc5'), __('% Comments &#187;', 'cc5')); ?> 
 <?php if (get_the_tags()) { ?>
                 <div class="postTags">
 <?php
@@ -47,15 +47,15 @@ $category = get_category($cat); ?>
             if ( function_exists('wp_pagenavi') ) {
                 wp_pagenavi();
             } else {
-                posts_nav_link(' &mdash; ', 'previous page', 'next page');
+                posts_nav_link(' &mdash; ', __('previous page', 'cc5'), __('next page', 'cc5'));
             }
             ?>
             </div>
 
             <div id="archives">
 
-			<strong><a href="/<?php echo $category->slug; ?>/rss">Subscribe to RSS</a></strong><br/><br/>
-<!--			  <h4>Archives</h4> 
+			<strong><a href="/<?php echo $category->slug; ?>/rss"><?php _e('Subscribe to RSS', 'cc5'); ?></a></strong><br/><br/>
+<!--			  <h4><?php _e('Archives', 'cc5'); ?></h4> 
               <ul class="archives">
                 <?php cc_get_cat_archives($cat, 'monthly', '', 'html', '', '', TRUE); ?>
               </ul>
