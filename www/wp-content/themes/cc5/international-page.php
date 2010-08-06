2cd7c98c0c29212ae099deaad0594431780be75c
nkinkade <nkinkade@committer.creativecommons.org>
2010-08-06 21:39:36 +0000

    Merged revisions 14611 via svnmerge from svn+ssh://svn@code.creativecommons.org/svnroot/ccwordpress/trunk
    ........
      r14611 | nkinkade | 2010-08-02 18:43:42 -0400 (Mon, 02 Aug 2010) | 1 line
      
      Updated international page to use new API call, obviating the need for the worldwide plugin.
    ........

--- www/wp-content/themes/cc5/international-page.php
+++ www/wp-content/themes/cc5/international-page.php
@@ -1,92 +1,126 @@
 <?php 
+
 // "Single" template will always, by definition, have a single post.
 // I'm quite sure this will not change, except on opposites day, perhaps.
-if (have_posts())  {
+if ( have_posts() )  {
     the_post(); 
 } else {
-  require (TEMPLATEPATH . '/404.php');
-  exit();
-} ?>
-<?php get_header(); ?>
-    <?php
-     // check worldwide categories
-     in_category(18) ? $is_worldwide_upcoming = true : $is_worldwide_upcoming = false;
-     in_category(19) ? $is_worldwide_in_progress = true : $is_worldwide_in_progress = false;
-     in_category(20) ? $is_worldwide_completed = true : $is_worldwide_completed = false;
-     //in_category(21) ? $is_worldwide = true : $is_worldwide = false;
+    require (TEMPLATEPATH . '/404.php');
+    exit();
+}
 
-     foreach ((get_the_category()) as $cat) {
-       if ($cat->category_parent == 21) {
-         $jurisdiction_name = $cat->cat_name;
-         $jurisdiction_code = $cat->category_nicename;
-       }
-     }
-    ?>
+get_header();
+
+// check worldwide categories
+in_category(18) ? $is_worldwide_upcoming = true : $is_worldwide_upcoming = false;
+in_category(19) ? $is_worldwide_in_progress = true : $is_worldwide_in_progress = false;
+in_category(20) ? $is_worldwide_completed = true : $is_worldwide_completed = false;
+//in_category(21) ? $is_worldwide = true : $is_worldwide = false;
+
+foreach ( get_the_category() as $cat ) {
+    if ( $cat->category_parent == 21 ) {
+        $jurisdiction_name = $cat->cat_name;
+        $jurisdiction_code = $cat->category_nicename;
+    }
+}
+
+$api_url = "http://api.creativecommons.org/rest/dev/license/standard/jurisdiction/";
+
+if ( $is_worldwide_completed ) {
+    $jurisdiction_dom = new DOMDocument();
+    $jurisdiction_dom->loadXML( file_get_contents($api_url . $jurisdiction_code) );
+
+    $jurisdiction_dom_root = $jurisdiction_dom->documentElement;
+    $jurisdiction_site = $jurisdiction_dom_root->getAttribute('local_url');
+    $license_elements = $jurisdiction_dom->getElementsByTagName('license');
+    $licenses = array();
+
+    for ( $i=0; $i < $license_elements->length; $i++ ) {
+        $license_url = $license_elements->item($i)->getAttribute('url');
+        $license_name = $license_elements->item($i)->getAttribute('name');
+        preg_match('/licenses\/(.*)\/\d\.\d/', $license_url, $matches);
+        $licenses[$matches[1]]['url'] = $license_url;
+        $licenses[$matches[1]]['name'] = $license_name;
+    }
   
-    <div id="mainContent" class="box single">
-      <div id="contentPrimary">
-    			<div class="block" id="title">
-						<h3 class="category">
-							<a href="<?php echo get_settings('home') . "/" ?>international">
-								Creative Commons International 
-							</a>
-						</h3>
+    ksort($licenses);
+}
 
-<? if ($jurisdiction_code != '') { ?>
-						<h2>
-							<img src="/images/international/<?php echo $jurisdiction_code ?>.png" alt="<?php echo $jurisdiction_code ?> flag" class="flag" /><?php 
-								the_title(); ?>
-							</h2>
-<? } ?>
-					</div>
-    
-            <div class="block international" id="post-<?php the_ID(); ?>">
-<? 
-     $license_xml = new LicenseXml();
+$home_settings = get_settings('home');
 
-     $jurisdiction_site = $license_xml->getJurisdictionSite($jurisdiction_code);
+echo <<<HTML
+<div id="mainContent" class="box single">
+    <div id="contentPrimary">
+        <div class="block" id="title">
+            <h3 class="category">
+                <a href="{$home_settings}/international">
+                    Creative Commons International 
+                </a>
+            </h3>
+HTML;
 
-     if ($jurisdiction_site) {
+$the_title = get_the_title();
+$the_ID = get_the_ID();
+
+if ( $jurisdiction_code != '' ) {
+    echo <<<HTML
+            <h2>
+                <img src="/images/international/{$jurisdiction_code}.png" alt="{$jurisdiction_code} flag" class="flag" /> 
+                $the_title
+            </h2>
+        </div>
+        <div class="block international" id="post-{$the_ID}">
+HTML;
+}
+
+if ( $jurisdiction_site ) {
+    echo <<<HTML
+        <div class="licensebox" style="margin:14px;">
+            Visit the <a href="{$jurisdiction_site}">jurisdiction's site</a>.
+        </div>
+HTML;
+}
+
+if ( $is_worldwide_completed ) {
+    echo <<<HTML
+        <div class="licensebox" style="margin:14px;">
+            <p>
+                The Creative Commons {$jurisdiction_name} licenses have now been integrated into
+                <a href="/choose/?jurisdiction={$jurisdiction_code}">the Creative Commons
+                licensing process</a>, so you are able to license your works under this
+                jurisdiction's law.
+            </p> 
+            <p>The latest version of the licenses available for this jurisdiction are:</p>
+            <ul>
+HTML;
+
+    foreach ( $licenses as $license ) {
+        echo "          <li><a href='{$license['url']}'>{$license['name']}</a></li>\n";
+    }
+
+    echo <<<HTML
+            </ul>
+            <p>
+                Many thanks to all who contributed to the license-porting process. This page 
+                remains for reference.
+            </p>
+            <p>
+                Please take a look at the mailing-list archive if you are interested in the 
+                academic discussion leading to the <span>{$jurisdiction_name}</span> 
+                final license.
+            </p>
+        </div>
+HTML;
+
+    the_content();
+
+    dynamic_sidebar('Single Post');
+
+    echo "  </div>";
+    echo "</div>";
+}
+
+get_sidebar();
+get_footer();
+
 ?>
-
-<div class="licensebox" style="margin:14px;">
-Visit the jurisdiction site <a href="<?=$jurisdiction_site?>">here</a>.
-</div>
-<? 
-     }
- 
-?>
-
-<? if ($is_worldwide_completed) { ?>
-              <div class="licensebox" style="margin:14px;">
-                <p>The <? echo $jurisdiction_name ?> license has now been integrated 
-                into <a href="/choose/?jurisdiction=<?php echo $jurisdiction_code ?>">the Creative 
-                Commons licensing process</a>, so you are able to license your works under this 
-                jurisdiction's law. </p> 
-                <p>The latest version of the licenses available for this jurisdiction are:</p>
-                <ul>
-                  <?php
-                          $licenses = $license_xml->getLicensesCurrent($jurisdiction_code);
-                          foreach ($licenses as $l) {
-                              $l[name] = $license_xml->getLicenseName($l[uri]);
-                              echo "<li><a href='$l[uri]'>$l[name]</a></li>\n";
-                          }
-                      /* } */ ?>
-                </ul>
-                <p>Many thanks to all who contributed to the license-porting process. This page 
-                remains for reference.</p>
-                <p>Please take a look at the mailing-list archive if you are interested in the 
-                academic discussion leading to the <span><?php echo $jurisdiction_name ?></span> 
-                final license.</p>
-              </div>
-              <? } ?>
-			  <?php the_content(); ?>
-
-        <?php 
-        	dynamic_sidebar('Single Post');
-        ?>
-
-          </div>
-</div>
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
