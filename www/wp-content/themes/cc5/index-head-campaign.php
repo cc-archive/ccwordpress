95c5453fea48a2b005f22d07975c00b89f494c14
bse <bse@committer.creativecommons.org>
2010-09-22 23:31:13 +0000

    Moved the #ccBlurb contents out into seperate files; index-head.php for standard page layout; index-head-campaign.php for campaign related layout.

---
+++ www/wp-content/themes/cc5/index-head-campaign.php
@@ -0,0 +1,12 @@
+<div id="splashBox" style="overflow: visible;">
+	<div id="splash">
+		<a href="http://zupport.creativecommons.org/donate?utm_source=ccorg&utm_medium=homepage_banner&utm_campaign=catalyst"><img src="/images/support/2010/cc-org-banner-Strength.png" border="0" align="center" alt="Invest in the future of creativity and knowledge. Donate Today." /></a>
+		<?php include ('progress.php'); ?>
+
+	</div>
+	<?php /* Show random Superhero here */ ?>
+	<div style="position:absolute; left: 15px; top: -10px; width:150px; height:189px;">
+		<img src="http://zupport.creativecommons.org/sites/default/files/hero-badge-robin-sloan.png" alt="Robin Sloan" />
+	</div>
+</div>
+
