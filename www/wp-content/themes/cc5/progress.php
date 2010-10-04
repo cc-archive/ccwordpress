91192b2e7287e9bba7152c8037d92d56abb43cea
bse <bse@committer.creativecommons.org>
2010-10-04 21:34:59 +0000

    Updated progress area to link to creativecommons.net and fall campaign utm_campaign value

--- www/wp-content/themes/cc5/progress.php
+++ www/wp-content/themes/cc5/progress.php
@@ -1,12 +1,12 @@
 <?php function showTestimonial() { 
-	$url = "https://support.creativecommons.org/testimonials";
-	$utm = "?utm_source=ccorg&utm_medium=testimonial";
+	$url = "https://creativecommons.net/testimonials";
+	$utm = "?utm_campaign=superhero&utm_source=ccorg&utm_medium=testimonial";
 ?>
 
 <div id="sidebarTestimonial">
 	<h3>Join Jonathan in supporting&nbsp;CC!</h3>
 	<p class="quote">
-		<a href="<?= $url ?><?= $utm ?>"><img src="https://support.creativecommons.org/images/75/jonathancoulton.jpg" align="left" alt="Jonathan Coulton" border="0" /></a>
+		<a href="<?= $url ?><?= $utm ?>"><img src="https://creativecommons.net/images/75/jonathancoulton.jpg" align="left" alt="Jonathan Coulton" border="0" /></a>
 		"With Creative Commons, the act of creation becomes not the end, but the beginning of a creative process that links complete strangers together in collaboration"
 	</p>
 	<p class="source">
@@ -14,7 +14,7 @@
 		<small>Musician</small><br/>
 	</p>
 </div>
-	<script>jQuery("#sidebarTestimonial").click(function() { window.location="https://support.creativecommons.org/donate<?= $utm ?>"; });</script>
+	<script>jQuery("#sidebarTestimonial").click(function() { window.location="https://creativecommons.net/donate<?= $utm ?>"; });</script>
 <?php 
 	/* end of showTestimonial() */
 	return; 
@@ -24,21 +24,21 @@
 function showThermometer() { 	
 
 	if (is_home()) {
-		$utm = "?utm_source=ccorg&utm_medium=homepage_thermometer";
+		$utm = "?utm_campaign=superhero&utm_source=ccorg&utm_medium=homepage_thermometer";
 	} else { 
-		$utm = "?utm_source=ccorg&utm_medium=thermometer"; 
+		$utm = "?utm_campaign=superhero&utm_source=ccorg&utm_medium=thermometer"; 
 	} 
 ?>
 
    			<div id="campaign">  
-				<div class="progress <?php if (is_home()) {?>home<?}?>" onclick="window.location='https://support.creativecommons.org/donate<?= $utm ?>';">
+				<div class="progress <?php if (is_home()) {?>home<?}?>" onclick="window.location='https://creativecommons.net/donate<?= $utm ?>';">
 					<div class="inner"><span>&nbsp;</span></div>
 				</div>
-				<?php if (is_home()) { ?><div class="homeGoal"><a href="https://support.creativecommons.org/donate<?= $utm ?>">$100,000</a></div><? } ?>
+				<?php if (is_home()) { ?><div class="homeGoal"><a href="https://creativecommons.net/donate<?= $utm ?>">$550,000</a></div><? } ?>
 				<div class="results<?php if (is_home()) {?>Home<?}?>">
-					<a href="https://support.creativecommons.org/donate<?= $utm ?>">
+					<a href="https://creativecommons.net/donate<?= $utm ?>">
 					<?php if (is_home()) { ?><strong><?php cc_progress_total() ?> Raised</strong> &mdash; Thank you!<?php } else { ?>
-						<?php cc_progress_total() ?> / $100,000 by&nbsp;Dec&nbsp;31 
+						<?php cc_progress_total() ?> / $550,000 by&nbsp;Dec&nbsp;31 
 						<br/>
 						<em>Help us reach our goal!</em>
 					<?php } ?>
