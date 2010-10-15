b96e7a9f22a88d2342d9bdf2e846f490ba142552
bse <bse@committer.creativecommons.org>
2010-10-15 20:51:45 +0000

    Added first round of superheroes for rotation in ccorg homepage

--- www/wp-content/themes/cc5/index-head-campaign.php
+++ www/wp-content/themes/cc5/index-head-campaign.php
@@ -1,3 +1,17 @@
+<?php
+/* Superhero cards */
+
+$hero[0][0] = "robin-sloan"; $hero[0][1] = "robin";
+$hero[1][0] = "elizabeth-stark"; $hero[1][1] = "elizabeth";
+$hero[2][0] = "mitchell-baker"; $hero[2][1] = "mitchell";
+$hero[3][0] = "neeru-khosla"; $hero[3][1] = "neeru";
+$hero[4][0] = "gautam-john"; $hero[4][1] = "gautam";
+$hero[5][0] = "andrew-rens"; $hero[5][1] = "andrew";
+
+$i = rand(0, count($hero));
+
+?>
+
 <div id="splashBox" style="overflow: visible;">
 	<div id="splash">
 <script>utmx_section("Banner")</script>
@@ -7,7 +21,7 @@
 	</div>
 	<div id="superhero">
 		<div id="superheroCard">
-			<a href="https://creativecommons.net/superheroes/neeru-khosla/"><img src="/images/support/2010/superheroes/superhero-home-neeru.png" alt="Neeru Khosla" border="0"/></a>
+		<a href="https://creativecommons.net/superheroes/<?= $hero[i][0] ?>/?utm_campaign=superhero&utm_source=ccorg&utm_medium=<?= $hero[i][1] ?>"><img src="/images/support/2010/superheroes/superhero-home-<?= $hero[i][1] ?>.png" alt="<?= $hero[i][0] ?>" border="0"/></a>
 		</div>
 	</div>
 </div>
