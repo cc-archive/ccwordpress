c9ccac720c2b7408d9d5ed8d8b5deb55593d9f2a
bse <bse@committer.creativecommons.org>
2010-10-25 21:09:29 +0000

    Hide 'Alumni' from the sidebar

--- www/wp-content/themes/cc5/sidebar.php
+++ www/wp-content/themes/cc5/sidebar.php
@@ -23,7 +23,7 @@
   </div>
   
   <?php
-  	$exclude_list = "23670,7486,7476,7471,7472,7473,7506,7474,7475,7487,7505,7682,7793,7794,12354,7499,7502,7501";
+  	$exclude_list = "23670,7486,7476,7471,7472,7473,7506,7470,7474,7475,7487,7505,7682,7793,7794,12354,7499,7502,7501";
   	$list_pages_query = "&title_li=&echo=0&exclude=".$exclude_list;
   	if ($post->post_parent) {
   		if ($root_post_id = get_post_meta($post->ID, "root", true)) {
