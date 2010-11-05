23417c778967bd50d1ce229786fb1ed1eefe4b37
bse <bse@committer.creativecommons.org>
2010-11-05 22:31:47 +0000

    Merged revisions 14752 via svnmerge from svn+ssh://svn@code.creativecommons.org/svnroot/ccwordpress/trunk
    ........
      r14752 | bse | 2010-11-05 22:30:59 +0000 (Fri, 05 Nov 2010) | 1 line
      
      Added 5th banner that displays the current thermometer and amount raised (via ajax)
    ........

--- www/wp-content/themes/cc5/site.js
+++ www/wp-content/themes/cc5/site.js
@@ -1,5 +1,5 @@
 function bannerHtml() {
-    var banner = new Array(3); for (i = 0; i < banner.length; ++i) banner[i] = new Array(2);
+    var banner = new Array(4); for (i = 0; i < banner.length; ++i) banner[i] = new Array(2);
     banner[0][0] = "&utm_medium=sbanner_1"; banner[0][1] = "Help the world harness the power of Creative Commons.";
     banner[1][0] = "&utm_medium=sbanner_2"; banner[1][1] = "Help the world harness the amazing strength of Creative Commons!";
     banner[2][0] = "&utm_medium=sbanner_3"; banner[2][1] = "Creative Commons, saving the world from failed sharing!";
@@ -10,14 +10,31 @@ function bannerHtml() {
 function thundercats() {
     if ((location.href.match(/http\:(.*)?creativecommons.org\/$/) && !location.href.match(/pport.creativecommons.org\/$/)) || location.href.match(/creativecommons.org\/donate/)) return;
 
-    var i = Math.floor(Math.random() * 4);
+    var i = Math.floor(Math.random() * 5);
     var banners = bannerHtml();
 
     var d = document.createElement("div");
     var mainContent = document.getElementById("globalWrapper");
     d.setAttribute('style', 'font-size: 22px; font-family: "helvetica neue", arial, sans-serif; line-height:1; text-shadow: 0 1px 0 rgba(255, 255, 255, 0.4); color: #ffff00; padding: 7px 0 2px 0; border-bottom: 1px solid rgb(120, 159, 44); margin-top: -1px; background: #c5deed; background:-webkit-gradient(linear, left top, left bottom, from(#c6e1f2), to(#84bddf)); background:-moz-linear-gradient(center top, #c5deed, #91b6cd); z-index:1000;');
-    d.innerHTML = '<a href="https://creativecommons.net/donate?utm_campaign=superhero&utm_source=ccorg' + banners[i][0] + '" style="color:#000; text-decoration:none;"><img src="https://creativecommons.net/sites/default/themes/cc/images/superhero/supercc-banner.png" style="vertical-align:middle; padding-right: 10px;" border="0"/> <strong><span style="">' + banners[i][1] + '</span></strong> &mdash; <em style="color:#c01100;">Donate Now</em></a>';
-    mainContent.parentNode.insertBefore(d, mainContent);
+	if (i < 4) {
+    	d.innerHTML = '<a href="https://creativecommons.net/donate?utm_campaign=superhero&utm_source=ccorg' + banners[i][0] + '" style="color:#000; text-decoration:none;"><img src="https://creativecommons.net/sites/default/themes/cc/images/superhero/supercc-banner.png" style="vertical-align:middle; padding-right: 10px;" border="0"/> <strong><span style="">' + banners[i][1] + '</span></strong> &mdash; <em style="color:#c01100;">Donate Now</em></a>';
+    	mainContent.parentNode.insertBefore(d, mainContent);
+	} else {
+		var utm="?utm_campaign=superhero&utm_source=ccorg&utm_medium=sbanner_progress";
+		d.innerHTML = '<div id="campaign" style="vertical-align:middle; color:#111; background:transparent; height: auto;"><div class="progress" style="width: 300px; vertical-align:middle; display:inline-block; margin: 0;" onclick="window.location=\'https://creativecommons.net/donate'+utm+'\'"><div class="inner"><span>&nbsp;</span></div></div> <span style="padding-left:10px; vertical-align:middle;"><span id="campaignRaised">&nbsp;</span> &mdash; <a href="https://creativecommons.net/donate'+utm+'"><em style="color:#c01100">Donate Now</em></a></span></div>';
+		d.style.display = "none";
+		mainContent.parentNode.insertBefore(d, mainContent);
+
+		var r = new XMLHttpRequest();
+		r.open('GET', '/includes/total.txt', true);
+		r.onreadystatechange = function(e) { 
+			if (r.readyState == 4) { 
+				document.getElementById("campaignRaised").innerHTML = r.responseText + ' <small>of $550,000</small> raised'; 
+				d.style.display = "block"; 
+			} 
+		};
+		r.send(null);
+	}
 }
 
 if (typeof window.addEventListener !== 'undefined') {
