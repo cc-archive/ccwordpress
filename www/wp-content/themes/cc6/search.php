69ac0ef18f8cf7d92962a670227924ba6b1cda67
bse <bse@committer.creativecommons.org>
2011-01-07 22:02:31 +0000

    Merged revisions 14899 via svnmerge from svn+ssh://svn@code.creativecommons.org/svnroot/ccwordpress/trunk
    ........
      r14899 | bse | 2011-01-07 22:01:09 +0000 (Fri, 07 Jan 2011) | 1 line
      
      Fixed site search implementation
    ........

--- www/wp-content/themes/cc6/search.php
+++ www/wp-content/themes/cc6/search.php
@@ -1,48 +1,30 @@
-<?php
-/*
- * Theme Name: 960 Base Theme
- * Theme URI: http://960basetheme.kiuz.it
- * Description: Wordpress theme based on 960 Grid System
- * Author: Domenico Monaco
- * Author URI: http://www.kiuz.it
- * Version: 0.5
- */
-?>
-
 <?php get_header(); ?>
 
-	<?php if (have_posts()) : ?>
+		<div id="title" class="container_16">
+			<h1 class="grid_16">
+				Search Results	
+			</h1>
+		</div>
 
-		<h2 class="pagetitle">Risultati della ricerca per "<?php the_search_query(); ?>"</h2>
-
-		<?php while (have_posts()) : the_post(); ?>
-
-		<div class="post post-front-page">
-			<h2><span class="data_t"><?php the_time('d.m.y') ?> </span><img src="<?php bloginfo('template_directory'); ?>/i/row_data.jpg" /> <a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-            
-			<div class="postmetadata">Categorie: <?php the_category(', ') ?> | Tags:<?php the_tags('<br />Tags: ', ', ', ''); ?></div>
-			<div class="entry">
-				<?php the_excerpt(); ?>
+		<div id="content">
+			<div class="container_16">
+				<div class="grid_12">
+					<div id="cse" style="padding-bottom: 25px; width: 100%;"><h3><img src="/wp-content/themes/cc5/images/loading.gif" alt="Loading" style="margin-bottom:-6px;" /> Loading search...</h3></div>
+					<script src="http://www.google.com/jsapi" type="text/javascript"></script>
+					<script type="text/javascript">
+						google.load('search', '1', {language : 'en'});
+						google.setOnLoadCallback(function() {
+								var customSearchControl = new google.search.CustomSearchControl('010316592082702087679:vmxqegqb1uy');
+								customSearchControl.setResultSetSize(google.search.Search.FILTERED_CSE_RESULTSET);
+								customSearchControl.draw('cse');
+								customSearchControl.execute('<?php echo htmlspecialchars($_GET['s']); ?>');
+						}, true);
+					</script>
+					<link rel="stylesheet" href="/wp-content/themes/cc5/google-cse.css" type="text/css" />
+				</div>
 			</div>
-            <h4 class="add_comment"><?php comments_popup_link('Aggiungi un commento ', '1 Commento presente ', '% Commenti presenti '); ?></h4>
 		</div>
 
-
-		<?php endwhile; ?>
-
-  	<div class="navigation">
-			<?php if(function_exists('wp_pagenavi')) { wp_pagenavi(); } else { ?>
-			<div class="alignleft"><?php next_posts_link('&laquo; Vecchie Voci') ?></div>
-			<div class="alignright"><?php previous_posts_link('Nuove Voci &raquo;') ?></div>
-			<?php } ?>
-		</div>
-
-	<?php else : ?>
-
-		<h2 class="center">Non trovato. Effettua una nuova ricerca.</h2>
-		<?php include (TEMPLATEPATH . '/searchform.php'); ?>
-
-	<?php endif; ?>
-
-<?php get_sidebar(); ?>
+	</div>
 <?php get_footer(); ?>
+
