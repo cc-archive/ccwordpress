2f1c483701c4bcf7899a36cf05a5ce78b220c995
nkinkade <nkinkade@committer.creativecommons.org>
2012-05-07 22:25:51 +0000

    Added Google Analytics tracking code to header

--- www/wp-content/themes/open4us.org/header.php
+++ www/wp-content/themes/open4us.org/header.php
@@ -67,6 +67,23 @@
 	 */
 	wp_head();
 ?>
+
+<!-- Begin Google Analytics -->
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-2010376-25']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<!-- End Google Analytics -->
+
 </head>
 
 <body <?php body_class(); ?>>
